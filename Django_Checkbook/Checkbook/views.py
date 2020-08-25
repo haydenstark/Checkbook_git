7287a9f3eff1a77b517e7cb52ecdce617ec9c880
Hayden Stark <haydenstark@aol.com>
2020-08-25 15:46:07 -0500

    Add Checkbook

---
+++ Django_Checkbook/Checkbook/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .forms import AccountForm, TransactionForm
+
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+def create_account(request):
+    form = AccountForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/CreateNewAccount.html', content)
+
+def balance(request):
+    return render(request, 'checkbook/BalanceSheet.html')
+
+def transaction(request):
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/AddTransaction.html', content)
